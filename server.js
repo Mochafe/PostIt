3183ebc5c1eb6b743c6527b43160c7920ab22c73
Mochafe <61238007+Mochafe@users.noreply.github.com>
2022-07-22 20:49:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/getAllPosts", (req, res) => {
 
 app.get("/getPost", (req, res) => {
     postModel.getPost(req.query.id).then((response) => {
-        console.log(response[0].dataValues.title)
+        console.log(typeof(response[0].dataValues.title))
         if(response[0].dataValues.title == []) res.send("Whala trick pa avek lé id la");
         res.send({
             title: response[0].dataValues.title,
